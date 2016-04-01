f17d143238ffcfc863f307605f50f10d8eb8e1ff
Nat Young <nat.young7@gmail.com>
2016-04-01 13:11:30 +1100

    fsgkljsadhgfads

--- go.sh
+++ go.sh
@@ -1,6 +1,7 @@
+#! /usr/bin/env bash
+
 git remote add -t \* -f yourface git@github.com:ThinTim/coffee-api-challenge.git
 git checkout master
 git pull yourface master --no-edit
 bundle update
 ./go serve
-
