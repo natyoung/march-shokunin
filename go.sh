5f9a56715f62cbe78256628dfaa0c5577c90d22c
Nat Young <natyoung@users.noreply.github.com>
2016-04-01 14:32:44 +1100

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-git remote add -t \* -f yourface git@github.com:ThinTim/coffee-api-challenge.git
+git remote add -t \* -f yourface https://github.com/ThinTim/coffee-api-challenge.git
 git checkout master
 git pull yourface master --no-edit
 bundle update --quiet
